1e5cfe892f6faf44f6f0360e6963bc68698e1c87
balestp <balestp@G96ZXZ1>
2017-05-30 13:45:26 -0400

    Add project files.

---
+++ MyTrains.Core/App.cs
@@ -0,0 +1,33 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Localization;
+using MvvmCross.Platform;
+using MvvmCross.Platform.IoC;
+using MyTrains.Core.Utility;
+using MyTrains.Localization;
+
+namespace MyTrains.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            //a bulk registration with the Inversion of Control (IOC) container.
+            CreatableTypes()
+                .EndingWith("Repository")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            Mvx.RegisterSingleton<IMvxTextProvider>
+                (new ResxTextProvider(Strings.ResourceManager));
+
+            RegisterAppStart(new AppStart());
+        }
+    }
+}
